bdfd315e1edcc3429faaa43ea70ce97c9f96a17e
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-05-12 13:06:45 +0200

    add validation\icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,12 @@
+import { FaSquarePhone } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa6";
+
 export default function Contact({ contact: { name, number, id }, onDelete }) {
     return (
         <div>
             <div>
-                <p>{name}</p>
-                <p>{number}</p>
+                <p><FaUser />{name}</p>
+                <p><FaSquarePhone />{number}</p>
             </div>
             <button onClick={() => onDelete(id)}>Delete</button>
         </div>
